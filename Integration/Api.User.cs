9114a8ce4309a01d83b66da352bb0cab43ac7f5b
rodrigo groff <rodrigo.groff@vortigo.com.br>
2019-05-17 16:51:04 -0300

    ajustes

---
+++ Integration/Api.User.cs
@@ -0,0 +1,56 @@
+using Gateway.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace Integration.Gateway
+{
+    [TestClass]
+    public class UserAPI : BaseTest
+    {
+        [TestMethod]
+        public void Authenticate()
+        {
+            var client = new RestClient(Gateway);
+            var request = new RestRequest("api/v1/user/authenticate", Method.POST);
+
+            request.AddJsonBody(new LoginInformation
+            {
+                Login = "x",
+                Passwd = "123"
+            });
+
+            IRestResponse response = client.Execute(request);
+
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                Assert.Fail("login nok");
+
+            var auth = JsonConvert.DeserializeObject<AuthenticatedUser>(response.Content);
+
+            if (string.IsNullOrEmpty(auth.Token))
+                Assert.Fail("auth.Token null");
+        }
+
+        [TestMethod]
+        public void GetUser()
+        {
+            string bearer = GetBearer("x", "123");
+
+            var client = new RestClient(Gateway);
+            var request = new RestRequest("api/v1/user/3", Method.GET);
+
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Authorization", "Bearer " + bearer);
+
+            IRestResponse response = client.Execute(request);
+
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                Assert.Fail("GetUser nok [1]");
+
+            var user = JsonConvert.DeserializeObject<AuthenticatedUser>(Cleanup(response.Content));
+
+            if (user.Name != "x")
+                Assert.Fail("GetUser nok [2]");
+        }
+    }
+}
---
+++ Integration/BaseTest.cs
@@ -0,0 +1,47 @@
+using Gateway.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace Integration.Gateway
+{
+    [TestClass]
+    public class BaseTest
+    {
+        public string Gateway = "http://localhost:18523";
+
+        public string GetBearer(string login, string pass)
+        {
+            #region - code - 
+
+            var client = new RestClient(Gateway);
+            var request = new RestRequest("api/v1/user/authenticate", Method.POST);
+
+            request.AddJsonBody(new LoginInformation
+            {
+                Login = login,
+                Passwd = pass
+            });
+
+            IRestResponse response = client.Execute(request);
+
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                Assert.Fail("login nok");
+
+            var auth = JsonConvert.DeserializeObject<AuthenticatedUser>(response.Content);
+
+            if (string.IsNullOrEmpty(auth.Token))
+                Assert.Fail("auth.Token null");
+
+            return auth.Token;
+
+            #endregion
+        }
+
+
+        public string Cleanup(string src)
+        {
+            return src.Replace("\\\"", "\"").TrimStart('\"').TrimEnd('\"');
+        }
+    }
+}
