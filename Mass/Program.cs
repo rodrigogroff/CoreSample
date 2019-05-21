808489b4e4e28033677ead330d9423a71f7a183d
rodrigo groff <rodrigo.groff@vortigo.com.br>
2019-05-21 09:50:02 -0300

    ajustes

---
+++ Mass/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Text;
+using Dapper;
+
+namespace Mass
+{
+    class Program
+    {
+        static string GetNome(ref List<string> lstPrimeirosNomes, ref List<string> lstSobrenomes)
+        {
+            var str = "";
+
+            var r = new Random();
+
+            int its = 1;
+            if (r.Next(1, 10) > 8) its = 2;
+
+            for (int i = 0; i < its; i++)
+                str += lstPrimeirosNomes[r.Next(0, lstPrimeirosNomes.Count)] + " ";
+
+            its = 1;
+
+            if (r.Next(1, 10) > 6) its = 2;
+            if (r.Next(1, 10) > 9) its = 3;
+
+            for (int i = 0; i < its; i++)
+                str += lstSobrenomes[r.Next(0, lstSobrenomes.Count)] + " ";
+
+            return str.TrimEnd();
+        }
+
+        static void Main(string[] args)
+        {
+            List<string> lstPrimeirosNomes = new List<string>();
+            List<string> lstSobrenomes = new List<string>();
+
+            using (var sr = new StreamReader("nomes.txt"))
+            {
+                var arrNomes = sr.ReadToEnd().Replace("\r\n", "¨").Replace("\n", "¨").Split('¨');
+
+                foreach (var item in arrNomes)
+                {
+                    if (item.StartsWith("-") || item == "")
+                        continue;
+
+                    int iPos = 0;
+                    foreach (var iNome in item.Split(' '))
+                    {
+                        if (iNome.Length > 2)
+                            if (!Char.IsNumber(iNome[0]))
+                            {
+                                if (iPos < 2)
+                                    lstPrimeirosNomes.Add(iNome);
+                                else
+                                    lstSobrenomes.Add(iNome);                               
+                            }
+                        iPos++;
+                    }                    
+                }
+            }
+
+            using (var db = new SqlConnection("Data Source=WIN-VC8RCO6KBEK\\SQLEXPRESS;Initial Catalog=VortigoServicePlatform;Integrated Security=SSPI;"))
+            {
+                db.Query("truncate table [Client]");
+                db.Query("truncate table [Admin]");
+                db.Query("truncate table [User]");
+            }
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                using (var db = new SqlConnection("Data Source=WIN-VC8RCO6KBEK\\SQLEXPRESS;Initial Catalog=VortigoServicePlatform;Integrated Security=SSPI;"))
+                {
+                    db.Query("insert into [Client] (Name) values (@Name)", new { Name = "Client" + i });
+                }
+            }
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                using (var db = new SqlConnection("Data Source=WIN-VC8RCO6KBEK\\SQLEXPRESS;Initial Catalog=VortigoServicePlatform;Integrated Security=SSPI;"))
+                {
+                    db.Query("insert into [Admin] (Name,Email,Password,ClientID) values (@Name,@Email,@Password,@ClientID)", 
+                        new { Name = GetNome(ref lstPrimeirosNomes, ref lstSobrenomes), Email = "dba@client"+ i + ".com", Password = "123456", ClientID = i });
+
+                    StringBuilder sb = new StringBuilder();
+
+                    for (int j = 1; j <= 100000; j++)
+                    {
+                        sb.AppendLine("insert into [User] (Name,Email,Password,ClientID) values ('" + GetNome(ref lstPrimeirosNomes, ref lstSobrenomes) + "', 'user" + j + "@client" + i + ".com', '123456', " + i + ");");
+
+                        if (j % 1000 == 0)
+                        {
+                            Console.WriteLine(i + "."+ j);
+                            db.Query(sb.ToString());
+                            sb.Clear();
+                        }                        
+                    }
+                }
+            }
+        }
+    }
+}
